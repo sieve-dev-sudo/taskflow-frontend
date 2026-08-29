4830569df8fd8d215bda204899763d1c6ea8bb48
sieve-dev-sudo <sieve.dev.kh@gmail.com>
2026-08-30 01:47:59 +0700

    feat: add enter/exit animations to task list

--- taskflow-frontend/src/components/TaskList.jsx
+++ taskflow-frontend/src/components/TaskList.jsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from 'framer-motion'
 import TaskItem from './TaskItem'
 
 function TaskList({ tasks }) {
@@ -11,9 +12,19 @@ function TaskList({ tasks }) {
 
   return (
     <div className="space-y-2">
-      {tasks.map((task) => (
-        <TaskItem key={task.id} task={task} />
-      ))}
+      <AnimatePresence>
+        {tasks.map((task) => (
+          <motion.div
+            key={task.id}
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, x: 50 }}
+            transition={{ duration: 0.2 }}
+          >
+            <TaskItem task={task} />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   )
 }
