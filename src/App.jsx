74bb864dd1e3f0491fdf94a4d018f49ff100f71f
sieve-dev-sudo <sieve.dev.kh@gmail.com>
2026-09-22 01:44:08 +0700

    feat: add /trash route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './pages/Home'
 import About from './pages/About'
+import Trash from './pages/Trash'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/trash" element={<Trash />} />
       </Routes>
     </Layout>
   )
